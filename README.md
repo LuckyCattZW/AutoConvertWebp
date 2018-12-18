d0037faace5ae7f61bbae574474826171d909d36
Zhi.Wei <249414003@qq.com>
2018-12-18 16:00:19 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AutoConvertWebp
 bat 自动执行转换.webp
+
+- 原始图片路径`\sroucePath`，放入你的原始.png或.jpg 图片
+- webp输出路径`\outWebpImages` 输出压缩完毕的.webp 图片路径
+- 自动转换.bat脚本`auto_convert_webp.bat`
+
+#### 使用方式
+- 将你的需要压缩的.png或.jpg 放入\sourePath路径内
+- 执行自动转换脚本
+- 生成的.webp图片将存储至\outWebpImages目录内
+
+#### 修改压缩比例
+- 编辑`auto_convert_webp.bat` 修改`set compress=15`
---
+++ auto_convert_webp.bat
@@ -0,0 +1,38 @@
+@echo off&setlocal enabledelayedexpansion
+
+rem 压缩比例 1～100，值越小压缩率约高
+set compress=15
+
+rem 原始图片路径
+set sroucePath=%~dp0sourceImages\
+
+rem webp输出路径
+set outPath=%~dp0outWebpImages\
+
+rem 工具路径
+set toolPath=%~dp0bin\
+
+echo=
+echo 遍历所有sroucePath目录下的.png与.jpg文件
+echo 并执行cwebp转换命令
+echo=
+
+rem 统计路径字符串数量
+set /a max=8190,min=0
+for /l %%a in (1,1,14) do (
+     set /a "num=(max+min)/2"
+     for /f "delims=" %%b in ("!num!") do if "!sroucePath:~%%b!" equ "" (set /a max=num) else set /a min=num
+)
+if "!sroucePath:~%num%!" neq "" set /a num+=1
+::echo 经计算字符串sroucePath共有%num%个字符
+
+rem 遍历路径下的所有png\jpg文件
+for /r %sroucePath% %%f in (*.png,*.jpg) do (
+set filename=%%f
+rem 执行转换命令
+%toolPath%cwebp -q %compress% !filename! -o %outPath%!filename:~%num%, -4!.webp
+)
+
+echo=
+echo 已经修改完毕！！！
+exit
